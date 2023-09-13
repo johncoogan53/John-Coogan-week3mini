dc23c774daf993dfab717fd672aa55c7d6c7fc24
johncoogan53 <141885470+johncoogan53@users.noreply.github.com>
2023-09-13 02:08:42 +0000

    "whatever this is broken"

--- pythonproject/src/desc_stat_polars.py
+++ pythonproject/src/desc_stat_polars.py
@@ -1,11 +1,11 @@
 """importing polars for data manipulation and pyplot for plotting"""
-import polars as pl
+import pythonproject.src.lib1 as lib1
 import matplotlib.pyplot as plt
 
-df = pl.read_csv("https://www.statlearning.com/s/Auto.csv", ignore_errors=True)
-print(df.describe())
+df = lib1.load_data_from_csv("https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv")
+print(lib1.summary_statistics(df))
 
-plt.scatter(df["displacement"], df["mpg"], alpha=0.5)
+plt.scatter(df["disp"], df["mpg"], alpha=0.5)
 plt.title("Vehicle MPG vs Displacement")
 plt.xlabel("Displacement")
 plt.ylabel("MPG")
--- pythonproject/tests/test_module.py
+++ pythonproject/tests/test_module.py
@@ -7,8 +7,15 @@ import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
 sys.path.insert(0, parent_dir)
+print(sys.path)
+exit()
 
+"""Import files from src we want to test"""
 from src import source_code
+from src import desc_stat_polars
+from pythonproject.src.lib1 import load_data_from_csv
+
+
 
 
 class TestSourceCode(unittest.TestCase):
@@ -18,6 +25,16 @@ class TestSourceCode(unittest.TestCase):
         """Test the add function"""
         result = source_code.add(1, 1)
         expected_result = 2
+        self.assertEqual(result, expected_result)
+
+    def test_load_data_from_csv(self):
+        """We will attempt to load some not very good csv files and some missing ones to ensure proper error handling"""
+        result = lib.load_data_from_csv("notAfile.csv")
+        expected_result = None
+        self.assertEqual(result, expected_result)
+
+        result = lib.load_data_from_csv("pythonproject\tests\testBreaker.md")
+        expected_result = None
         self.assertEqual(result, expected_result)
 
 
